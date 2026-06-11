6c47e210bbe2c9c9498517087185a9968bdd83d4
Aryan Saini <aaryansaini2005@gmail.com>
2026-06-11 12:30:38 +0530

    result

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ public class Demo {
     public static void main(String[] args) {
         System.err.println("Hello world!");
         System.out.println("Bye!");
+        System.out.println("Expecting result");
     }
 }
